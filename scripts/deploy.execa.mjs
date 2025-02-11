d05acafe15d0100f0ae4f9ccf36a30c5ee8ae618
Eric Moore <EricGittaMoore@duck.com>
2025-02-11 23:12:18 +0800

    Refactor build script and update dependencies to use execa package

--- scripts/deploy.execa.mjs
+++ scripts/deploy.execa.mjs
@@ -1,32 +1,29 @@
-import { execa as $ } from '@rebundled/execa'
+import * as execa from 'execa'
 import process from 'node:process'
 import path from 'node:path'
 import fs from 'node:fs'
 
 const cwd = process.cwd()
+const $ = execa.$
+const $$ = $({ stdout: 'inherit', stderr: 'inherit', verbose: 'full' }).sync
 
-// 克隆仓库
-await $('git', ['clone', 'git@github.com:eric-gitta-moore/tagspaces.git', '-b', 'develop'])
+$$`rm -rf tagspaces`
+$$`git clone https://github.com/eric-gitta-moore/tagspaces.git -b develop`
 
-// // 切换目录并安装依赖
-// process.chdir('tagspaces')
-// await $('npm', ['ci'])
+process.chdir('tagspaces')
+$$`npm ci`
 
-// // 写入配置文件
-// const extconfig = `
-// window.ExtIsFirstRun = false;
-// window.ExtCheckForUpdatesOnStartup = false;
-// window.ExtSaveLocationsInBrowser = true;
-// window.ExtUseSidecarsForFileTagging = true;
-// window.ExtLocations = [
-//     ${process.env.ExtLocations}
-//   ]
-// `
+const extconfig = `
+window.ExtIsFirstRun = false;
+window.ExtCheckForUpdatesOnStartup = false;
+window.ExtSaveLocationsInBrowser = true;
+window.ExtUseSidecarsForFileTagging = true;
+window.ExtLocations = [
+    ${process.env.ExtLocations}
+  ]
+`
 
-// fs.writeFileSync(path.join(cwd, 'tagspaces', 'web/extconfig.js'), extconfig)
+fs.writeFileSync(path.join(cwd, 'tagspaces', 'web/extconfig.js'), extconfig)
 
-// // 准备web文件
-// await $('npm', ['run', 'prepare-web'])
-
-// // 复制web目录到dist
-// await $('cp', ['-r', 'web', '../dist'])
+$$`npm run prepare-web`
+$$`cp web ../dist`
